a1eab1508c72d47b290df9efed1efe2bd0b1ed88
EnzoPaalman <35348@ma-web.nl>
2023-01-09 20:34:14 +0100

    game

---
+++ game/player.js
@@ -0,0 +1,71 @@
+export default class Player
+{
+
+    rightPressed = false;
+    leftPressed = false;
+
+    constructor(canvas, velocity)
+    {
+        this.canvas = canvas;
+        this.velocity = velocity;
+
+        this.x = this.canvas.width / 2;
+        this.y = this.canvas.height - 75;
+        this.width = 80;
+        this.height = 67;
+        this.image = new Image();
+        this.image.src = "img/jet.png";
+
+        document.addEventListener("keydown", this.keydown);
+        document.addEventListener("keyup", this.keyup);
+    }
+
+    draw(ctx)
+    {
+        this.move();
+        this.collideWithWalls();
+        ctx.drawImage(this.image, this.x, this.y, this.width, this.height)
+    }
+
+    collideWithWalls()
+    {
+        //left
+        if(this.x < 0) {
+            this.x = 0;
+        }
+
+        //right
+        if(this.x > this.canvas.width - this.width){
+            this.x = this.canvas.width - this.width;
+        }
+    }
+
+    move(){
+        if(this.rightPressed)
+        {
+            this.x += this.velocity;
+        }
+        else if(this.leftPressed)
+        {
+            this.x += -this.velocity;
+        }
+    }
+
+    keydown = event =>{
+        if(event.code == 'ArrowRight'){
+            this.rightPressed = true;
+        }
+        if(event.code == 'ArrowLeft'){
+            this.leftPressed = true;
+        }
+    }
+
+    keyup = event =>{
+        if(event.code == 'ArrowRight'){
+            this.rightPressed = false;
+        }
+        if(event.code == 'ArrowLeft'){
+            this.leftPressed = false;
+        }
+    }
+}
